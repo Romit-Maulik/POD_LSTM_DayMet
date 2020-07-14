21d3cd046457fcea93aa70235aeaca70440afbb8
Romit Maulik <rmaulik@anl.gov>
2020-07-14 07:46:57 +0000

    Updated shell script

--- run.sh
+++ run.sh
@@ -4,9 +4,11 @@
 #COBALT -A Performance
 #COBALT -t 2:00:00
 
-# Activate python
-source activate daymet_env
+# Activate python - first Taylor's JLSE Horovod build
+source /gpfs/jlse-fs0/projects/datascience/parton/mlcuda/mconda/setup.sh
+# Then my package additions
+conda activate /home/rmaulik/link_dir/rmaulik/DayMet/daymet_gpu_env
+
 cd src/
 python ROM.py --geo_data tmax --comp cae --train_mode --train_space --win 7
 cd ..
-source deactivate
